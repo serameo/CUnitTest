7d3315bdd8eb887c995e52789a9188828ff30698
Seree Rakwong <meo.rakwong@hotmail.com>
2021-11-12 08:46:30 +0700

    Create c_unittest.h

---
+++ include/c_unittest.h
@@ -0,0 +1,15 @@
+#ifndef __c_unittest_h__
+#define __c_unittest_h__
+
+/*
+ *
+ * File Name: c_unittest.h
+ * Author: Seree Rakwong
+ * Date: 12-NOV-2021
+ *
+ */
+
+#include "test_safedb.h"
+
+
+#endif /* __ets_unittest_h__ */
