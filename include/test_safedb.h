692fed279b2999308d158bd4ada65cadf16048fc
Seree Rakwong <meo.rakwong@hotmail.com>
2021-11-12 08:54:26 +0700

    Create test_safedb.h

---
+++ include/test_safedb.h
@@ -0,0 +1,17 @@
+#ifndef __test_safedb_h__
+#define __test_safedb_h__
+
+/*
+ *
+ * File Name: test_safedb.h
+ * Author: Seree Rakwong
+ * Date: Fri 12 15:31:49 GMT 2021
+
+ * Purpose:
+ *   Test safedb object
+ *
+ */
+
+void test_safedb_run(char* title);
+
+#endif /* __test_safedb_h__ */
